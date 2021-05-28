95542da0c7e78a878815edcb2defabdf5680725f
Garry Joss <73390442+gj0-dev@users.noreply.github.com>
2021-05-28 18:51:06 +0100

    added knope file

---
+++ knope.js
@@ -0,0 +1,13 @@
+// npm i knope
+
+
+// const knope = require('knope')
+ 
+// // Pass in the name as a parameter
+// var compliment = knope.getCompliment('Ann')
+ 
+// console.log(compliment)
+
+
+// Get three adjectives before the animal for Ann, because she's great.
+// var compliment = knope.getCompliment('Ann', 3)
