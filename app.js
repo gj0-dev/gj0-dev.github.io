210b402b0807b5527b84312c7d2bf13abced2b30
Garry Joss <gj0@outlook.com>
2021-06-02 14:12:43 +0100

    added app.js for typewriter text effect

--- app.js
+++ app.js
@@ -14,3 +14,16 @@
 //     document.querySelector("body").style.backgroundColor = '#000000';
 // };
 
+let i = 0;
+let txt = 'IT Professional'; /* The text */
+let txt2 = 'Aberdeen, Scotland'; /* The text */
+let speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("profession").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+typeWriter();
+
